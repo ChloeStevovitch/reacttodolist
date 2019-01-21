5f0db139a96a44809ef25e592105b86c06d98b2b
Chloe Stevovitch <cjgs@free.fr>
2019-01-21 15:49:30 -0500

    Possible to add new tasks (name only)

--- src/App.js
+++ src/App.js
@@ -2,23 +2,68 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
+const Tasks =['faire des courses','aller au cours de chant'];
+
+
+function Toarray(props) {
+    var array = props.tab.map(elmt => <li>{elmt}</li>);
+    return <ul>{array}</ul>
+}
+
+function Totitle(props) {
+    return <h1>{props.message}</h1>
+}
+
+function Livepreview(props){
+    return <ul><li>{props.value}</li></ul>
+}
+
+
 class App extends Component {
-  render() {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        if(this.state.value!='') {
+            Tasks.unshift(this.state.value);
+            this.state.value='';
+            document.getElementById("taskInput").value='';
+        }
+        event.preventDefault();
+
+    }
+
+    render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+            <Totitle message='My todolist'/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Toarray tab={[this.state.value].concat(Tasks)}/>
+              <form  onSubmit={this.handleSubmit}>
+                  <label>
+                      Name:
+                      <input id='taskInput' type="text" value={this.state.value} onChange={this.handleChange} />
+                  </label>
+                  <input type="submit" value="Save" />
+              </form>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
